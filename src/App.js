fed48b34686a16804810353131093ca0186df4d6
Ana Carolina <ana_carolina_88@live.com>
2021-06-08 15:28:28 -0300

    header styles, add Login files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login/*" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
