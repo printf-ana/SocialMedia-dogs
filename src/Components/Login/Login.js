cda97b0ade177857d1f3d2459b5d6cfc58d40270
Ana Carolina <ana_carolina_88@live.com>
2021-06-10 15:13:58 -0300

    login updates

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,12 +1,16 @@
 import React from "react";
-import { Route, Routes } from "react-router";
+import { Navigate, Route, Routes } from "react-router";
+import { UserContext } from "../../UserContext";
 import LoginCreate from "./LoginCreate";
 import LoginForm from "./LoginForm";
 import LoginPasswordLost from "./LoginPasswordLost";
 import LoginPasswordReset from "./LoginPasswordReset";
 
-
 const Login = () => {
+  const { login } = React.useContext(UserContext);
+  if (login === true) {
+    return <Navigate to="conta" />;
+  }
   return (
     <div>
       <Routes>
