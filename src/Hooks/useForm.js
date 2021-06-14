1f55411f29bff031c5fb42c3836e2b61466d0499
Ana Carolina <ana_carolina_88@live.com>
2021-06-14 10:24:39 -0300

    finish login and register

--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -6,6 +6,11 @@ const types = {
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     message: "Preencha com um email válido",
   },
+  password: {
+    regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
+    message:
+      "A senha precisa ter no mínimo 8 caracteres, 1 maiúsculo, 1 minúsculo e 1 dígito. ",
+  },
 };
 
 const useForm = (type) => {
